4089184853ec1f88016631fdf5210b94e439ef9c
Guilherme Andrade <g@gandrade.net>
2023-06-24 14:52:40 +0100

    Start considering CI

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule Fpe.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:dev], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
       {:recon, "~> 2.3", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
