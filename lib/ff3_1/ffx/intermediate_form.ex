4eab6420c8511afd31bee6f8b6951d95e7fd1eae
Guilherme Andrade <g@gandrade.net>
2023-08-06 13:36:50 +0100

    Work around unexplained lack of coverage report of defstruct statement
    We'll likely benefit from marginal performance improvements, anyway.

--- lib/ff3_1/ffx/intermediate_form.ex
+++ lib/ff3_1/ffx/intermediate_form.ex
@@ -3,35 +3,42 @@ defmodule FF3_1.FFX.IntermediateForm do
   @moduledoc false
   import Bitwise
 
-  @enforce_keys [:radix, :mask, :bits_per_symbol, :perfect_fit]
-  defstruct [:radix, :mask, :bits_per_symbol, :perfect_fit]
+  require Record
 
-  @opaque ctx :: %__MODULE__{
-            radix: FF3_1.FFX.radix(),
-            mask: pos_integer,
-            bits_per_symbol: pos_integer,
-            perfect_fit: boolean
-          }
+  Record.defrecordp(:ctx, [
+    :radix,
+    :mask,
+    :bits_per_symbol,
+    :perfect_fit
+  ])
+
+  @opaque ctx ::
+            record(:ctx,
+              radix: FF3_1.FFX.radix(),
+              mask: pos_integer,
+              bits_per_symbol: pos_integer,
+              perfect_fit: boolean
+            )
 
   @spec new_ctx(FF3_1.FFX.radix()) :: ctx()
   def new_ctx(radix) do
     precise_bits_per_symbol = :math.log2(radix)
     bits_per_symbol = ceil(precise_bits_per_symbol)
 
-    %__MODULE__{
+    ctx(
       radix: radix,
       mask: (1 <<< bits_per_symbol) - 1,
       bits_per_symbol: bits_per_symbol,
       perfect_fit: bits_per_symbol == precise_bits_per_symbol
-    }
+    )
   end
 
   @spec left_pad_and_revert(ctx, non_neg_integer, non_neg_integer) :: non_neg_integer
   def left_pad_and_revert(ctx, number, tail_padding) do
-    if ctx.perfect_fit do
+    if ctx(ctx, :perfect_fit) do
       perfect_lpr_recur(
-        ctx.mask,
-        ctx.bits_per_symbol,
+        ctx(ctx, :mask),
+        ctx(ctx, :bits_per_symbol),
         tail_padding,
         number,
         _acc = 0,
@@ -39,7 +46,7 @@ defmodule FF3_1.FFX.IntermediateForm do
       )
     else
       imperfect_lpr_recur(
-        ctx.radix,
+        ctx(ctx, :radix),
         tail_padding,
         number,
         _acc = 0,
