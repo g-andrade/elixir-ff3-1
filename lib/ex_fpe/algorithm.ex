8d2fe965a53ce46dd034734473fb5a7cc50c26ab
Guilherme Andrade <g@gandrade.net>
2026-07-13 21:13:19 +0100

    Remove internal `Algorithm` module from docs

--- lib/ex_fpe/algorithm.ex
+++ lib/ex_fpe/algorithm.ex
@@ -1,4 +1,6 @@
 defprotocol ExFPE.Algorithm do
+  @moduledoc false
+
   @spec do_encrypt_or_decrypt(t, tweak, input, encrypt?) :: {:ok, output} | {:error, reason}
         when tweak: binary(), input: binary(), encrypt?: boolean, output: binary(), reason: term()
   def do_encrypt_or_decrypt(t, tweak, input, encrypt?)
